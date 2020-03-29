5e7d19270e7e09a2819fde30de1df6bc8e3d9806
fernandopso <fernandopso3@gmail.com>
2020-03-29 16:10:00 -0300

    Updates

---
+++ precache-manifest.dd439ae96be6349166d6b5b3bbe241d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42732cb844f2264865a9c58c92628d3",
+    "url": "/coon/index.html"
+  },
+  {
+    "revision": "c1f09b6dfcba6d30b9de",
+    "url": "/coon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d064f2162920363e9012",
+    "url": "/coon/static/js/2.cb5637e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coon/static/js/2.cb5637e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f09b6dfcba6d30b9de",
+    "url": "/coon/static/js/main.dc7531db.chunk.js"
+  },
+  {
+    "revision": "0b8f962a0846e0b0e94c",
+    "url": "/coon/static/js/runtime-main.99d17075.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coon/static/media/logo.5d5d9eef.svg"
+  }
+]);
