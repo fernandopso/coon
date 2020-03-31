df3c638b09c74aaf0e275f8d872509e7d2dc3436
fernandopso <fernandopso3@gmail.com>
2020-03-30 22:09:57 -0300

    Updates

---
+++ precache-manifest.2b8b43e3febbfb9a29902bd10bdab8f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9562b48f738d58ab1087d0c6054a40",
+    "url": "/quiz-movie/index.html"
+  },
+  {
+    "revision": "74b022912a24d3b1a292",
+    "url": "/quiz-movie/static/css/main.ea691f19.chunk.css"
+  },
+  {
+    "revision": "46259b8f32324230a901",
+    "url": "/quiz-movie/static/js/2.34a6dd88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-movie/static/js/2.34a6dd88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b022912a24d3b1a292",
+    "url": "/quiz-movie/static/js/main.33a34c13.chunk.js"
+  },
+  {
+    "revision": "c4b4e8752f0676071bd7",
+    "url": "/quiz-movie/static/js/runtime-main.2a7ef66d.js"
+  },
+  {
+    "revision": "2fb8c88a590c24c3b1947c15dc74455a",
+    "url": "/quiz-movie/static/media/movies.2fb8c88a.yaml"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coon/precache-manifest.3fbf40f1a3213722d747dc439ba6547d.js"
+  "/quiz-movie/precache-manifest.2b8b43e3febbfb9a29902bd10bdab8f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coon/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quiz-movie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
