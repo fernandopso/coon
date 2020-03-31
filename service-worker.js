b428e8510b34f1153cee0a59f0fa3bb1600aaed8
fernandopso <fernandopso3@gmail.com>
2020-03-30 22:23:48 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quiz-movie/precache-manifest.2b8b43e3febbfb9a29902bd10bdab8f2.js"
+  "/quiz-movie/precache-manifest.5d61b01aa6a9484f60ba390b8530a767.js"
 );
 
 self.addEventListener('message', (event) => {
