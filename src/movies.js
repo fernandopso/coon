7853550186c1e56587a4d58b7e989c86b9dd2d32
fernandopso <fernandopso3@gmail.com>
2020-04-13 22:24:50 -0300

    display carousel of movie

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import movies from './movies.json'
+import movies from './movies.json';
+import './carousel.css'
 
 class Movies extends React.Component {
-  renderPicture(picture) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgDisplayed: 0
+    }
+  }
+
+  renderImage(pic, index) {
     return (
-      <div>
-        <img src={picture.src} />
+      <figure className={index == this.state.imgDisplayed ? 'show' : 'hide'}>
+        <img className="center" src={pic.src} />
+      </figure>
+    )
+  };
+
+  nextImage(lastPicture) {
+    if (this.state.imgDisplayed < lastPicture) {
+      return this.state.imgDisplayed + 1
+    }
+  };
+
+  prevImage() {
+    if (this.state.imgDisplayed !== 0) {
+      return this.state.imgDisplayed - 1
+    }
+  };
+
+  slideShow(pictures) {
+    return (
+      <div className="slideshow">
+        {pictures.map((pic, index) => this.renderImage(pic, index))}
+        <span className="prev" onClick={() => this.setState({imgDisplayed: this.prevImage()})}>&laquo;</span>
+        <span className="next" onClick={() => this.setState({imgDisplayed: this.nextImage(pictures.length)})}>&raquo;</span>
       </div>
     )
   };
@@ -13,15 +43,14 @@ class Movies extends React.Component {
   renderMovie(movie) {
     return (
       <div>
-        {movie.title}
-        {movie.pictures.map(picture => this.renderPicture(picture))}
+        {this.slideShow(movie.pictures)}
       </div>
     )
   };
 
   render() {
     return (
-      <div>{movies.map(movie => this.renderMovie(movie))}</div>
+      <div>{this.renderMovie(movies[1])}</div>
     )
   };
 }
