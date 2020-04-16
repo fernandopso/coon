c47671295ef80c7d52b3f8eb7da81f27d685b9cf
fernandopso <fernandopso3@gmail.com>
2020-04-15 21:03:21 -0300

    create class for carousel

---
+++ src/carousel.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './carousel.css'
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgDisplayed: 0
+    }
+  }
+
+  renderImage(pic, index) {
+    return (
+      <figure className={index == this.state.imgDisplayed ? 'show' : 'hide'}>
+        <img className="center" src={pic.src} />
+      </figure>
+    )
+  };
+
+  nextImage(lastPicture) {
+    if (this.state.imgDisplayed < lastPicture) {
+      return this.state.imgDisplayed + 1
+    }
+  };
+
+  prevImage() {
+    if (this.state.imgDisplayed !== 0) {
+      return this.state.imgDisplayed - 1
+    }
+  };
+
+  slideShow(pictures) {
+    return (
+      <div className="slideshow">
+        {pictures.map((pic, index) => this.renderImage(pic, index))}
+        <span className="prev" onClick={() => this.setState({imgDisplayed: this.prevImage()})}>&laquo;</span>
+        <span className="next" onClick={() => this.setState({imgDisplayed: this.nextImage(pictures.length)})}>&raquo;</span>
+      </div>
+    )
+  };
+
+  render() {
+    return (
+      <div>
+        {this.slideShow(this.pros.movie.pictures)}
+      </div>
+    )
+  };
+}
+
+export default Carousel;
--- src/movies.js
+++ src/movies.js
@@ -1,56 +1,19 @@
 import React from 'react';
 import movies from './movies.json';
-import './carousel.css'
+import Carousel from './carousel.js'
 
 class Movies extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      imgDisplayed: 0
+    this.movies = {
+      total: movies.length,
+      sample: movies[3]
     }
   }
 
-  renderImage(pic, index) {
-    return (
-      <figure className={index == this.state.imgDisplayed ? 'show' : 'hide'}>
-        <img className="center" src={pic.src} />
-      </figure>
-    )
-  };
-
-  nextImage(lastPicture) {
-    if (this.state.imgDisplayed < lastPicture) {
-      return this.state.imgDisplayed + 1
-    }
-  };
-
-  prevImage() {
-    if (this.state.imgDisplayed !== 0) {
-      return this.state.imgDisplayed - 1
-    }
-  };
-
-  slideShow(pictures) {
-    return (
-      <div className="slideshow">
-        {pictures.map((pic, index) => this.renderImage(pic, index))}
-        <span className="prev" onClick={() => this.setState({imgDisplayed: this.prevImage()})}>&laquo;</span>
-        <span className="next" onClick={() => this.setState({imgDisplayed: this.nextImage(pictures.length)})}>&raquo;</span>
-      </div>
-    )
-  };
-
-  renderMovie(movie) {
-    return (
-      <div>
-        {this.slideShow(movie.pictures)}
-      </div>
-    )
-  };
-
   render() {
     return (
-      <div>{this.renderMovie(movies[4])}</div>
+      <div>{this.movies.total}</div>
     )
   };
 }
