a7e88a935e2335bb773b57265d1f411904a88a68
fernandopso <fernandopso3@gmail.com>
2020-04-17 20:20:10 -0300

    render next and prev buttons

--- src/carousel.js
+++ src/carousel.js
@@ -4,38 +4,48 @@ import './carousel.css'
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.totalPictures = this.props.movie.pictures.length
+    this.pictures = this.props.movie.pictures.slice(0,5)
     this.state = {
       imgDisplayed: 0
     }
   };
 
+  prevImage() {
+    return this.state.imgDisplayed - 1
+  };
+
+  prevButton() {
+    if (this.state.imgDisplayed !== 0) {
+      return <span className="prev" onClick={() => this.setState({imgDisplayed: this.prevImage()})}>&laquo;</span>
+    }
+  };
+
+  nextImage() {
+    return this.state.imgDisplayed + 1
+  };
+
+  nextButton() {
+    if (this.state.imgDisplayed < this.pictures.length - 1) {
+      return <span className="next" onClick={() => this.setState({imgDisplayed: this.nextImage()})}>&raquo;</span>
+    }
+  };
+
   renderImage(pic, i) {
     return (
-      <figure className={i == this.state.imgDisplayed ? 'show' : 'hide'}>
+      <figure className={i === this.state.imgDisplayed ? 'show' : 'hide'}>
         <img className="center" src={pic.src} alt="" />
       </figure>
     )
   };
 
-  nextImage() {
-    if (this.state.imgDisplayed < this.totalPictures) {
-      return this.state.imgDisplayed + 1
-    }
-  };
-
-  prevImage() {
-    if (this.state.imgDisplayed !== 0) {
-      return this.state.imgDisplayed - 1
-    }
-  };
-
   render() {
     return (
-      <div className="slideshow">
-        {this.props.movie.pictures.map((pic, i) => this.renderImage(pic, i))}
-        <span className="prev" onClick={() => this.setState({imgDisplayed: this.prevImage()})}>&laquo;</span>
-        <span className="next" onClick={() => this.setState({imgDisplayed: this.nextImage()})}>&raquo;</span>
+      <div>
+        <div className="slideshow">
+          {this.pictures.map((pic, i) => this.renderImage(pic, i))}
+        </div>
+        {this.prevButton()}
+        {this.nextButton()}
       </div>
     )
   };
